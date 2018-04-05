101558903c3dd118f9678a262eba387feb238181
Dennis Luxen <info@project-osrm.org>
2018-04-05 21:40:32 +0200

    examiner 2.0.11

--- Formula/exomizer.rb
+++ Formula/exomizer.rb
@@ -1,8 +1,8 @@
 class Exomizer < Formula
   desc "6502 compressor with CBM PET 4032 support"
   homepage "https://bitbucket.org/magli143/exomizer/wiki/Home"
-  url "https://bitbucket.org/magli143/exomizer/wiki/downloads/exomizer-2.0.10.zip"
-  sha256 "74afb08a51466e24e5e95b8672c9a152a1ab4eb31464f29cd630c1feb585b027"
+  url "https://bitbucket.org/magli143/exomizer/wiki/downloads/exomizer-2.0.11.zip"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   bottle do
     cellar :any_skip_relocation
